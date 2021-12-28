29acee9335496d28940d347a47561baac799e1d8
wannit9 <espoir980921@gmail.com>
2021-12-28 16:55:13 +0900

    21.12.28 commit5
    최종 수정 !!

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
 $(function(){
-    
-})
+    $(".btn2").click(function(){
+        $("#popup1").fadeIn(200);
+    });
+    $(".close").click(function(){
+        $("#popup1").fadeOut(200);
+    });
+
+    $(".btn3").click(function(){
+        $("#popup2").fadeIn(200);
+    });
+    $(".close").click(function(){
+        $("#popup2").fadeOut(200);
+    });
+
+    $(".btn4").click(function(){
+        $("#popup3").fadeIn(200);
+    });
+    $(".close").click(function(){
+        $("#popup3").fadeOut(200);
+    });
+});
