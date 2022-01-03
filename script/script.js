804084508eea1e44b1b31abc50a83f2a0268934e
wannit9 <espoir980921@gmail.com>
2022-01-03 16:48:04 +0900

    22.01.03 commit

--- script/script.js
+++ script/script.js
@@ -19,4 +19,20 @@ $(function(){
     $(".close").click(function(){
         $("#popup3").fadeOut(200);
     });
+
+    const saTriggerMargin = 400;
+      const saElementList = document.querySelectorAll('.sa');
+
+      const saFunc = function() {
+        for (const element of saElementList) {
+          if (!element.classList.contains('show')) {
+            if (window.innerHeight > element.getBoundingClientRect().top + saTriggerMargin) {
+              element.classList.add('show');
+            }
+          }
+        }
+      }
+
+      window.addEventListener('load', saFunc);
+      window.addEventListener('scroll', saFunc);
 });
